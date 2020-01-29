b764b32c0e1771cb9d47f538192bac16e0d064ed
Jasmine Terry <jasmine.terry91@gmail.com>
2020-01-29 15:01:49 -0500

    intial form create

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom'
 import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
 import Movie from "./Movies/Movie";
